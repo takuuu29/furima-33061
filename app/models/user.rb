cc97826b04c5c09d7fd6254aec3cdca21dbfb03a
takuuu29 <75967873+takuuu29@users.noreply.github.com>
2021-01-29 16:58:16 +0900

    ユーザー管理機能と単体テストコードの実装

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: {with: /\A[一-龥ぁ-ん]/ }
+  validates :first_name, presence: true, format: {with: /\A[一-龥ぁ-ん]/ }
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root to: "furima#index"
+  devise_for :users
+  root to: "items#index"
+  resources :items, only: [:index, :create, :new] do
+  end
 end
