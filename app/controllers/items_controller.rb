1438a6176c7640997f88e868927e4f4b998cb909
takuuu29 <75967873+takuuu29@users.noreply.github.com>
2021-02-11 13:26:50 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :move_to_log_in, except: [:index]
 
   def index
+    @items = Item.all.includes(:user).order('created_at DESC')
   end
 
   def new
