5b0584635d18f1142ea631a9691f1dc7b03cd021
takuuu29 <75967873+takuuu29@users.noreply.github.com>
2021-02-17 13:53:43 +0900

    商品情報編集機能 修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.includes(:user).order('created_at DESC')
@@ -23,7 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    
     unless user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     end
